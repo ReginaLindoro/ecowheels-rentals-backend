37e8307012725fd7ceb32c13d16f9c990bb34dd8
Regina Lindoro <reginalindoro@Reginas-Air.lan1>
2023-07-27 01:37:05 -0500

    Added API to update MongoDB with new hardware availability values

--- app.py
+++ app.py
@@ -1,21 +1,105 @@
 # app.py
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+import bson.json_util as json_util
 import certifi
-ca = certifi.where()
+import json
 
 app = Flask(__name__)
 
-# MongoDB configuration
-client = MongoClient("mongodb+srv://reginalindoro:JsUcVf06cZLTr37d@cluster0.oxbejdf.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
-db = client.test
+ca = certifi.where()
 
-@app.route('/insert', methods=['POST'])
-def insert_data():
-    data = request.get_json()
-    collection = db['test_collection']  # Replace with the collection name
-    inserted_data = collection.insert_one(data)
-    return jsonify(str(inserted_data.inserted_id))
+#TODO: take configurations outside
+# MongoDB configuration
+MONGO_URI = 'mongodb+srv://{DB_USER}:{DB_USER_PASS}@adapdb.g2igjno.mongodb.net/?retryWrites=true&w=majority'
+DB_NAME = 'ADAPdb'
+DB_USER = 'ADAP'
+DB_USER_PASS = 'ADAP'
+
+# Connect to database
+client = MongoClient(MONGO_URI, tlsCAFile=ca)
+db = client[DB_NAME]
+collection = db['Resources']
+
+# This method gets hardware information from the database
+def getHWSet():
+    hwSetData = collection.find()
+    return json_util.dumps(hwSetData)
+        
+# This method updates the database to reflect new available value of hardware
+# when user checks in/checks out number of units specified by quantity
+@app.route('/placeorder/<user_hw_request>', methods=['POST'])
+def handleResources(user_hw_request):
+    # user_hw_request = { #for testing. remove
+    #     "hardwareSet1": {
+    #         "quantity": 5
+    #     },
+    #     "hardwareSet2": {
+    #         "quantity": 5
+    #     },
+    #     "type": "checkout"
+    # }
+    type = user_hw_request['type']
+    err = {
+        "error": {
+            "code":100,
+            "message": "Error!"
+        }
+    }
+
+    data = getHWSet()
+    db_hardware_sets = json.loads(data)
+    # set hardwareSets variables
+    capacity_hwSet1 = db_hardware_sets[0]['Capacity']
+    capacity_hwSet2 = db_hardware_sets[1]['Capacity']
+    availability_hwSet1 = db_hardware_sets[0]['Available']
+    availability_hwSet2 = db_hardware_sets[1]['Available']
+    checkedOut_hwSet1 = capacity_hwSet1 - availability_hwSet1
+    checkedOut_hwSet2 = capacity_hwSet2 - availability_hwSet2
+
+    # loop through user hardware request information
+    for key, value in user_hw_request.items():
+        if isinstance(value, dict) and "quantity" in value:
+            # if hardware quantity is greater than 0, set variables for the hardware
+            if value['quantity'] > 0:
+                quantity = value['quantity']
+                hwSet = key[-1]
+                if hwSet == '1':
+                    availability = availability_hwSet1
+                    checkedOut = checkedOut_hwSet1
+                elif hwSet == '2':
+                    availability = availability_hwSet2
+                    checkedOut = checkedOut_hwSet2
+
+                # If user requests to check in, they can only check in hardware quantity that is 
+                # less than or equal to checked out. Then add quantity to availability
+                # If user requests to check out, they can only check out hardware quantity that is 
+                # less than or equal to availability. Then subtract availability from quantity
+                if type == "checkin":
+                    if quantity <= int(checkedOut):
+                        availability += quantity
+                    else:
+                        return err
+                elif type == "checkout":
+                    if quantity <= int(availability):
+                        availability -= quantity
+                    else:
+                        return err
+                    
+                # call method to update the database    
+                updateDB(availability, hwSet)
+    
+    return 'Success' 
+
+# This method updates the database with new Availability values
+def updateDB(availability, hwSet):
+    try:
+        newValues = { '$set': { 'Available': availability } }
+        inserted_data = collection.update_one({'HardwareSet': hwSet}, newValues)
+        return str(inserted_data)
+    except:
+        print("An exception occurred :", e)
+        return e
 
 if __name__ == '__main__':
     app.run(debug=True)
